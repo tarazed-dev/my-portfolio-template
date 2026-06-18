2cc3d29ecb020c9c43c45c4f9b08f25b084bad37
tarazed-dev <tarazed.dev@gmail.com>
2026-06-19 00:40:10 +0300

    delete start server

--- OPEN-SITE.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-start "" "http://localhost:3000"
-echo Opening http://localhost:3000 in your browser...
-echo.
-echo If the page does not load, first double-click START.bat
-timeout /t 5 >nul
